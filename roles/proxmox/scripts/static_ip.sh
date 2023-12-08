5434893941f8197a272fdf058fd40c8b8a01c2c9
Kyle Galvin <kylethedeveloper@gmail.com>
2023-12-07 21:27:48 -0800

    saving

--- roles/proxmox/scripts/static_ip.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-qm set 201 -ipconfig0 gw=192.168.1.254,ip=192.168.1.211/24
-qm set 202 -ipconfig0 gw=192.168.1.254,ip=192.168.1.212/24
-qm set 203 -ipconfig0 gw=192.168.1.254,ip=192.168.1.213/24
